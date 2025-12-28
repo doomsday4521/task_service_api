c32c1951319d8200763cf422a78c174a7d9b21c0
Ritabrata Pal <rito3668@gmail.com>
2025-12-28 18:53:54 +0530

    minor fix

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from alembic import command
+from alembic.config import Config
+import os
 from app.api.routes.auth import router as auth_router
 import app.db.imports
 from app.api.routes.me import router as me_router
@@ -7,6 +10,14 @@ app =FastAPI(
     title="task service api",
     version="0.1.0"
 )
+@app.on_event("startup")
+async def run_migrations():
+    alembic_cfg = Config("alembic.ini")
+    alembic_cfg.set_main_option(
+        "sqlalchemy.url",
+        os.environ["DATABASE_URL"],
+    )
+    command.upgrade(alembic_cfg, "head")
 
 app.include_router(auth_router)
 app.include_router(me_router)
